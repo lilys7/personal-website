a0ee16a5cdf540c7a394e45f3ee3d073f9ffec22
Lily Shen <lily.shen@gdit.com>
2026-09-08 15:06:32 -0400

    feat: add resume and contact sections TBD

---
+++ src/sections/Contact.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderSection } from '@/components/PlaceholderSection'
+
+export function Contact() {
+  return <PlaceholderSection id="contact" title="Contact" />
+}
---
+++ src/sections/Resume.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderSection } from '@/components/PlaceholderSection'
+
+export function Resume() {
+  return <PlaceholderSection id="resume" title="Resume" />
+}
